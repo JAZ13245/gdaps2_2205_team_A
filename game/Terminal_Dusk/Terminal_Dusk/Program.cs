b139d30b39d8394470cec58c3e433e0086579aa4
evan riley <uluruecr@outlook.com>
2021-02-28 15:23:46 -0500

    Created mono game

---
+++ game/Terminal_Dusk/Terminal_Dusk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Terminal_Dusk
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}

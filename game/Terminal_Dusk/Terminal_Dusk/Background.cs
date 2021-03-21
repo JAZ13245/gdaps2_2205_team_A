753a704bf5be6831bf2873a429207c860764358a
evan riley <uluruecr@outlook.com>
2021-03-21 16:57:06 -0400

    Timer

--- game/Terminal_Dusk/Terminal_Dusk/Background.cs
+++ game/Terminal_Dusk/Terminal_Dusk/Background.cs
@@ -9,5 +9,21 @@ namespace Terminal_Dusk
 {
     class Background
     {
+        private Texture2D backgroundImg;
+        private Rectangle position;
+        
+        //constructor for background
+        public Background(Texture2D backImg, Rectangle position)
+        {
+            // Save copies/references to the info we'll need later
+            this.position = position;
+            this.backgroundImg = backImg;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the background
+            spriteBatch.Draw(backgroundImg, position, Color.White);
+        }
     }
+    
 }

ce281a24613b1ba617b818364c7ef9eb09bde05d
Caitlin Will <caw8317@rit.edu>
2021-03-21 17:58:03 -0400

    updated the player class.

--- game/Terminal_Dusk/Terminal_Dusk/Player.cs
+++ game/Terminal_Dusk/Terminal_Dusk/Player.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Terminal_Dusk
 {
-    // Mario State enum
+    // Player State enum
     enum PlayerState
     {
         FaceLeft,
@@ -19,7 +19,58 @@ namespace Terminal_Dusk
     }
     class Player
     {
-        Vector2 playerLoc;       // Mc's location on the screen
+        Vector2 playerLoc;  // Mc's location on the screen
         PlayerState state;
+
+        //for the character sheet
+        Texture2D spriteSheet;
+
+        //for the animation
+        int frame;
+        double timeCounter;
+        double fps;
+        double timePerFrame;
+
+        //constants for walk rectangles
+        const int WalkFrameCount = 4;
+        const int MainRectHeight = 37;
+        const int MainRectWidth = 10;
+
+        // Properties to get & set player's current location and animation state
+        public float X
+        {
+            get
+            {
+                return this.playerLoc.X;
+            }
+            set
+            {
+                this.playerLoc.X = value;
+            }
+        }
+
+        public PlayerState State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        //the constructor
+        public Player(Texture2D spriteSheet, Vector2 playerLoc, PlayerState startingState)
+        {
+            this.spriteSheet = spriteSheet;
+            this.playerLoc = playerLoc;
+            this.state = startingState;
+
+            // Initialize
+            fps = 10.0;                     // Will cycle through 10 walk frames per second
+            timePerFrame = 1.0 / fps;       // Time per frame = amount of time in a single walk image
+        }
+
+        //updating the player's animation
+        public void UpdateAnimation(GameTime gameTime)
+        {
+
+        }
     }
 }

b89a8fd82326b7514aedfabce8a2420bde60dab4
James Zilberman <jaz1324576@gmail.com>
2021-05-06 18:24:42 -0400

    Yikes

--- game/Terminal_Dusk/Terminal_Dusk/Enemies/Imp.cs
+++ game/Terminal_Dusk/Terminal_Dusk/Enemies/Imp.cs
@@ -143,9 +143,6 @@ namespace Terminal_Dusk
                 case EnemyState.IdleBehaviour:
                     break;
 
-                case EnemyState.Hostile:
-                    break;
-
                 case EnemyState.Attacking:
                     int randomDirection = enemyRNG.Next(0, 2);
                     if (randomDirection == 0)
@@ -178,11 +175,12 @@ namespace Terminal_Dusk
                             swoopSpeed--;
                         }
                     }
-                    if (swoopSpeed == 14)
+                    if (swoopSpeed == -14)
                     {
+                        swoopSpeed = 14;
                         currentState = EnemyState.Idle;
                     }
-                    position.Y -= swoopSpeed;
+                    position.Y += swoopSpeed;
                     swoopSpeed--;
                     break;
 
